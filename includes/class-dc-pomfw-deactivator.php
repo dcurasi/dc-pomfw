28bbc9ae876e2ac251e276851318fbc64050e06c
Dario Curasi <dariocurasi@libero.it>
2017-01-29 17:49:56 +0100

    base boilerplate

---
+++ includes/class-dc-pomfw-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/dcurasi
+ * @since      1.0.0
+ *
+ * @package    Dc_Pomfw
+ * @subpackage Dc_Pomfw/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Dc_Pomfw
+ * @subpackage Dc_Pomfw/includes
+ * @author     Dario Curasì <curasi.d87@gmail.com>
+ */
+class Dc_Pomfw_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
